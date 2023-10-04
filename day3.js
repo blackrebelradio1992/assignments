4e63d9b12753edcbd6c20160f019ef9c2e17d143
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-04 11:51:52 -0500

    new

--- day3.js
+++ day3.js
@@ -1,95 +1,112 @@
-// console.log("hey vic")
+// // console.log("hey vic")
 
-function makeAName(firstName, lastName) {
-    let fullName = firstName + " " + lastName
-    return fullName;
-}
+// function makeAName(firstName, lastName) {
+//     let fullName = firstName + " " + lastName
+//     return fullName;
+// }
 
-// console.log(makeAName("John", "Wick"));
+// // console.log(makeAName("John", "Wick"));
 
-// data types
-    // strings
+// // data types
+//     // strings
 
-    greeting = "This is a string and this is how i work"
+//     greeting = "This is a string and this is how i work"
 
-    let person = " mark"
+//     let person = " mark"
 
-    // console.log( greeting + person)
-    // console.log(`${person}`)
+//     // console.log( greeting + person)
+//     // console.log(`${person}`)
 
-    //numbers
+//     //numbers
 
-    // console.log(Math.floor(3242/3));
+//     // console.log(Math.floor(3242/3));
 
-    // booleans
-    // console.log ('' == false)
-    // console.log()
+//     // booleans
+//     // console.log ('' == false)
+//     // console.log()
 
-    // function xoTest(){
-    //     let x = 8
-    //     let o = 9
+//     // function xoTest(){
+//     //     let x = 8
+//     //     let o = 9
 
-    //     return x === o ? true : false;
-    // }
+//     //     return x === o ? true : false;
+//     // }
 
-    // console.log(xoTest())
+//     // console.log(xoTest())
 
-    // conparison operators
+//     // conparison operators
 
-    // let myArray = ["Name", `${35}`, 35, false];
-    // // console.log(myArray.slice(0,3));
+//     // let myArray = ["Name", `${35}`, 35, false];
+//     // // console.log(myArray.slice(0,3));
 
-    // let myArray2 = ["Name", `${35}`, 35, false,[4, 3, 1, "hoopla"]];
+//     // let myArray2 = ["Name", `${35}`, 35, false,[4, 3, 1, "hoopla"]];
 
-    // console.log(myArray2[4][3]);
-    // myArray2[4].push(3)
-    // console.log(myArray2)
+//     // console.log(myArray2[4][3]);
+//     // myArray2[4].push(3)
+//     // console.log(myArray2)
 
 
     
-    // loops
+//     // loops
 
-    // while, for Of, for in, for i,
+//     // while, for Of, for in, for i,
 
-    // let num = 100
+//     // let num = 100
 
-    // while (num >0){
-    //     console.log(num)
-    //     num -= 1
-    // }
-    // console.log("end of while loop")
+//     // while (num >0){
+//     //     console.log(num)
+//     //     num -= 1
+//     // }
+//     // console.log("end of while loop")
 
-    // for (let i = 10; i > 0; i--){
-    //     console.log(i)
-    // }
-    // console.log("end of for loop")
+//     // for (let i = 10; i > 0; i--){
+//     //     console.log(i)
+//     // }
+//     // console.log("end of for loop")
 
-    let myObj = {
-        "I": 1,
-        "II": 2
-    }
+//     let myObj = {
+//         "I": 1,
+//         "II": 2
+//     }
 
-    // for (key, value of Object.entries(myObj)){
-    //     console.log(key)
-    //     console.log(value)
-    // }
-    // for (numeral)
-    // let myString = "asdfghjkl;poiuygbnjkfghyg"
+//     // for (key, value of Object.entries(myObj)){
+//     //     console.log(key)
+//     //     console.log(value)
+//     // }
+//     // for (numeral)
+//     // let myString = "asdfghjkl;poiuygbnjkfghyg"
 
-    // let letters = {}
-    // for (ltr of myString){
-    //     console.log(letters[ltr])
-    //     if (letters[ltr]){
-    //         letters[ltr] =+ 1
-    //     }
-    //     else{
-    //         letters[ltr] = 1
-    //     }
-    // }
-    // console.log(letters)
+//     // let letters = {}
+//     // for (ltr of myString){
+//     //     console.log(letters[ltr])
+//     //     if (letters[ltr]){
+//     //         letters[ltr] =+ 1
+//     //     }
+//     //     else{
+//     //         letters[ltr] = 1
+//     //     }
+//     // }
+//     // console.log(letters)
 
-    const makeFullName = (firstName, lastName) => {
-         `${firstName} ${lastName}`
-        };
+// //     const makeFullName = (firstName, lastName) => {
+// //          `${firstName} ${lastName}`
+// //         };
 
-console.log(makeFullName("Benjamin", "Cohen")); // "Benjamin Cohen"
+// // console.log(makeFullName("Benjamin", "Cohen")); // "Benjamin Cohen"
+
+// const nums = [1, 2, 3];
+
+// // const doubles = nums.map((x) => x * 2);
+
+// // console.log(doubles); // [2, 4, 6]
+
+// // // const nums = [1, 2, 3];
+
+// const doubles = nums.map(doubler);
+
+// console.log(doubles); // [2, 4, 6]
+
+// function doubler(x) {
+//   return x * 2;
+// }
+
