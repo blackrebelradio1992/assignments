c2cf53f5e7bec4ac3eface23f02b0e385aba881b
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-04 09:47:16 -0500

    more notes

--- day3.js
+++ day3.js
@@ -1,1 +1,18 @@
-console.log("hey vic")
+// console.log("hey vic")
+
+function makeAName(firstName, lastName) {
+    let fullName = firstName + " " + lastName
+    return fullName;
+}
+
+// console.log(makeAName("John", "Wick"));
+
+// data types
+    // strings
+
+    greeting = "This is a string and this is how i work"
+
+    let person = " mark"
+
+    // console.log( greeting + person)
+    console.log(``)
